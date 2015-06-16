b6a6fc9c1a47662a7a58f7046f52a7fc17bcdd7d
Aaron Broad <aaron.broad@me.com>
2015-06-16 00:08:11 -0300

    #8: Make Con test repeatedly

--- test/distne_test.exs
+++ test/distne_test.exs
@@ -1,15 +1,17 @@
 defmodule DistneTest do
   use ExUnit.Case
 
-  test "Con forwards {:stim, amount} as {:stim, amount*weight}" do
+  test "Con repeatedly forwards {:stim, amount} as {:stim, amount*weight}" do
     weight = :random.uniform()
     {:ok, pid} = Distne.Net.Con.start_link(weight)
     {:ok, sink} = Distne.Net.TestProbe.start_link()
     :ok = GenServer.call(pid, {:set_sink, sink})
-    amount = :random.uniform()
-    GenServer.call(pid, {:stim, amount})
-    expected_sink_stim_amount = weight * amount
-    assert {:ok, {:stim, expected_sink_stim_amount}} == GenServer.call(sink, :received)
+    Enum.each(1..10, fn (_)  ->
+      amount = :random.uniform()
+      GenServer.call(pid, {:stim, amount})
+      expected_sink_stim_amount = weight * amount
+      assert {:ok, {:stim, expected_sink_stim_amount}} == GenServer.call(sink, :received)
+    end)
   end
 end
 
