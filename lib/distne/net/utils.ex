0e53fe983b47155c7216a730ebcb77137c074f90
Aaron Broad <aaron.broad@me.com>
2015-06-18 22:33:22 -0300

    #17: documented stim

--- lib/distne/net/utils.ex
+++ lib/distne/net/utils.ex
@@ -2,6 +2,9 @@ defmodule Distne.Net.Utils do
   @moduledoc """
   Utils is a collection of functions with common utility to to Net nodes
   """
+  @doc """
+  Stimulate a Net node identified by pid by amount
+  """
   def stim(pid, amount) do
     GenServer.call(pid, {:stim, amount})
   end
