4db1e97397a6cda9dd3547c734a5029bd2b2a9bf
Aaron Broad <aaron.broad@me.com>
2015-06-20 09:36:08 -0300

    #16: Increased coverage of TestProbe

--- lib/distne/net/test_probe.ex
+++ lib/distne/net/test_probe.ex
@@ -14,10 +14,6 @@ defmodule Distne.Net.TestProbe do
     GenServer.start_link(Distne.Net.TestProbe, {State, nil, nil})
   end
 
-  def handle_call({:stim, amount}, _from, {State, _, sent}) do
-    {:reply, :ok, {State, {:stim, amount}, sent}}
-  end
-
   def handle_call(:received, _from, {State, received, sent}) do
     {:reply, {:ok, received}, {State, received, sent}}
   end
@@ -27,7 +23,7 @@ defmodule Distne.Net.TestProbe do
     {:reply, :ok, {State, received, message}}
   end
 
-  def handle_call(:sent, _from, {State, received, sent}) do
-    {:reply, {:ok, sent}, {State, received, sent}}
+  def handle_call(message, _from, {State, _, sent}) do
+    {:reply, :ok, {State, message, sent}}
   end
 end
