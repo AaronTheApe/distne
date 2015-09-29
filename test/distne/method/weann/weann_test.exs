61f92c2c567c161b406b7a9c4cdef3c92766f6e5
AaronTheApe <aaron.broad@me.com>
2015-09-29 08:04:15 -0300

    in progress

--- test/distne/method/weann/weann_test.exs
+++ test/distne/method/weann/weann_test.exs
@@ -2,9 +2,13 @@ defmodule Distne.Method.Weann.WeannTest do
   use ExUnit.Case
 
   alias Distne.Method.Weann.Weann, as: Weann
+  alias Distne.Method.Weann.WeannSettings, as: WeannSettings
+  alias Distne.Net.TestProbe, as: TestProbe
 
   test "Weann solves bit parity" do
-    {:ok, weann} = Weann.start_link(%{})
+    settings = %WeannSettings{num_inputs: 3, num_hidden: 5, num_outputs: 1}
+    {:ok, weann} = Weann.start_link(settings)
     {:ok, client} = TestProbe.start_link
+    
   end
 end
