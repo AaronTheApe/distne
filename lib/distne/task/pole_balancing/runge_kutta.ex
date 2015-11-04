9f3e97937d5020dfd41c25268d4b22b6c43221f0
Aaron Broad <aaron.broad@me.com>
2015-11-04 19:14:07 -0400

    in progress work on single pole

--- lib/distne/task/pole_balancing/runge_kutta.ex
+++ lib/distne/task/pole_balancing/runge_kutta.ex
@@ -2,13 +2,13 @@ defmodule Distne.Task.PoleBalancing.RungeKutta do
   use GenServer
 
   defmodule State do
-    defstruct dydx: nil, y0: nil, h: nil, xi: nil, yi: nil
+    defstruct dydx: nil, y0: nil, h: nil, xi: nil, yi: nil, constants: nil
   end
 
   alias Distne.Task.PoleBalancing.RungeKutta
 
-  def start_link(dydx, y0, h) do
-    GenServer.start_link(RungeKutta, %State{dydx: dydx, y0: y0, h: h, xi: 0, yi: y0})
+  def start_link(dydx, y0, h, constants) do
+    GenServer.start_link(RungeKutta, %State{dydx: dydx, y0: y0, h: h, xi: 0, yi: y0, constants: constants})
   end
 
   def step(pid) do
@@ -16,28 +16,28 @@ defmodule Distne.Task.PoleBalancing.RungeKutta do
   end
 
   def handle_call(:step, _sender, state) do
-    kone = k1(state.dydx, state.xi, state.yi)
-    ktwo = k2(state.dydx, state.xi, state.yi, state.h)
-    kthree = k3(state.dydx, state.xi, state.yi, state.h)
-    kfour = k4(state.dydx, state.xi, state.yi, state.h)
+    kone = k1(state.dydx, state.xi, state.yi, state.constants)
+    ktwo = k2(state.dydx, state.xi, state.yi, state.h, state.constants)
+    kthree = k3(state.dydx, state.xi, state.yi, state.h, state.constants)
+    kfour = k4(state.dydx, state.xi, state.yi, state.h, state.constants)
     yi = state.yi + (1.0/6.0)*(kone + 2*ktwo + 2*kthree + kfour)*state.h
     xi = state.xi + state.h
     {:reply, {:ok, yi}, %State{state| xi: xi, yi: yi}}
   end
 
-  defp k1(dydx, xi, yi) do
-    dydx.(xi, yi)
+  defp k1(dydx, xi, yi, constants) do
+    apply(dydx, [xi, yi, constants])
   end
 
-  defp k2(dydx, xi, yi, h) do
-    dydx.(xi + 0.5*h, yi + 0.5*k1(dydx, xi, yi)*h)
+  defp k2(dydx, xi, yi, h, constants) do
+    apply(dydx, [xi + 0.5*h, yi + 0.5*k1(dydx, xi, yi, constants)*h, constants])
   end
 
-  defp k3(dydx, xi, yi, h) do
-    dydx.(xi + 0.5*h, yi + 0.5*k2(dydx, xi, yi, h)*h)
+  defp k3(dydx, xi, yi, h, constants) do
+    apply(dydx, [xi + 0.5*h, yi + 0.5*k2(dydx, xi, yi, h, constants)*h, constants])
   end
 
-  defp k4(dydx, xi, yi, h) do
-    dydx.(xi + h, yi + k3(dydx, xi, yi, h)*h)
+  defp k4(dydx, xi, yi, h, constants) do
+    apply(dydx, [xi + h, yi + k3(dydx, xi, yi, h, constants)*h, constants])
   end
 end
--- test/distne/task/pole_balancing/runge_kutta_test.exs
+++ test/distne/task/pole_balancing/runge_kutta_test.exs
@@ -3,14 +3,57 @@ defmodule RungeKuttaTest do
 
   alias Distne.Task.PoleBalancing.RungeKutta
 
-  def change_in_ball_temp(_x, y) do
+  def change_in_ball_temp(_x, y, _constants) do
     -2.2067*:math.pow(10, -12)*(:math.pow(y, 4) - 81*:math.pow(10, 8))
   end
 
   test "solves http://w3.gazi.edu.tr/~balbasi/mws_gen_ode_txt_runge4th.pdf example 3" do
-    {:ok, rk_pid} = RungeKutta.start_link(&change_in_ball_temp/2, 1200, 240)
+    {:ok, rk_pid} = RungeKutta.start_link(&change_in_ball_temp/3, 1200, 240, [])
     assert is_pid(rk_pid)
     assert {:ok, 675.6509511828407} = RungeKutta.step(rk_pid)
     assert {:ok, 594.912631110278} = RungeKutta.step(rk_pid)
+  end
+
+  defp sgn(x) do
+    if x > 0 do
+      1
+    else
+      if x < 0 do
+        -1
+      else
+        0
+      end
+    end
+  end
+
+  #TODO
+  # Simply Formulas
+  # Make effective formulas composed in acceleration formulas
+  # Make acceleration formulas take x, y, remaining variables in list
+  # simulate poll with no force but initial angle wrong
+  # watch it dance
+
+  defp eff_pm(m, angle) do
+    cos_angle = :math.cos(angle)
+    cos_angle_squared = cos_angle*cos_angle
+    m * (1 - 3 / 4 * cos_angle_squared)
+  end
+
+  defp eff_pf(m, l, velocity, angle, hinge_friction_coeff, gravity) do
+    term1 = mass*length*velocity*velocity*:math.sin(angle)
+    term2 = 3/4*mass*:math.cos(angle)*((hinge_friction_coeff*velocity)/(mass*length) + gravity*:math.sin(angle))
+    term1 + term2
+  end
+
+  defp x_accelleration(task_force, track_friction_coeff, x_velocity, eff_pole_force, cart_mass, eff_pole_mass) do
+    (task_force - cart_mass * sgn(x) + eff_pole_force)/(cart_mass + eff_pole_mass)
+  end
+
+  defp angular_acceleration(length, x_acc, angle, gravity, hinge_friction_coeff, velocity, pole_mass) do
+    (3 / (4*length))*(x_acc*:math.cos(angle) + gravity*:math.sin(angle) + (hinge_friction_coeff*velocity)/(pole_mass*length))
+  end
+
+  test "solves http://machinelearning.wustl.edu/mlpapers/paper_files/gomez08a.pdf single pole" do
+    
   end
 end
